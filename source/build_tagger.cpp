7d5ec6065d3aebb3cc66eb76bb43e16b879c3793
Steven Kester <a0080415@nus.edu.sg>
2014-09-28 21:15:12 +0800

    Implemented some functions

--- source/build_tagger.cpp
+++ source/build_tagger.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <map>
+#include <vector>
+#include <cstring>
 #define TAGSIZE 47
 using namespace std;
 
 map<string,int> tags;
 
-void setPOSTags(){
+void setTags(){
 	string pennTags[] = { "CC", "CD", "DT", "EX", "FW", "IN",
 			"JJ", "JJR", "JJS", "LS", "MD", "NN", "NNS", "NNP", "NNPS", "PDT",
 			"POS", "PRP", "PRP$", "RB", "RBR", "RBS", "RP", "SYM", "TO", "UH",
@@ -18,7 +22,7 @@ void setPOSTags(){
 	return;
 }
 
-int getPOSTagsIndex(string tag){
+int getTagIndex(string tag){
 	map<string,int>::iterator it;
 	it = tags.find(tag);
 	if(it==tags.end()){
@@ -29,6 +33,65 @@ int getPOSTagsIndex(string tag){
 	}
 }
 
-int main(){
+map<string,int> words;
+int tagTable[TAGSIZE][TAGSIZE];
+vector< vector<int> > wordTagTable;
+
+void insertWord(string word){
+	map<string,int>::iterator it;
+	it = words.find(word);
+	if(it==words.end()){
+		int index = words.size();
+		words.insert(pair<string,int>(word,index));
+		vector<int> emptyVec (TAGSIZE,0);
+		wordTagTable.push_back(emptyVec);
+	}
+	return;
+};
+
+int getWordIndex(string word){
+	map<string,int>::iterator it;
+	it = words.find(word);
+	if(it==words.end()){
+		return -1;
+	}
+	else{
+		return it->second;
+	}
+}
+
+
+void init(){
+	memset(tagTable,0,sizeof(tagTable));
+	wordTagTable.clear();
+	vector<int> emptyVec (TAGSIZE,0);
+	wordTagTable.push_back(emptyVec);
+}
+
+void countData(ifstream &infile){
+	string line;
+	while(getline(infile,line)){
+		string prevTag = "<s>";
+		istringstream istream(line);
+		string word, tag;
+		while((getline(istream,word,'/'))&&(getline(istream,tag,' '))){
+			int tagIndex = getTagIndex(tag);
+			int prevTagIndex = getTagIndex(prevTag);
+			tagTable[prevTagIndex][tagIndex]+=1;
+			insertWord(word);
+			int wordIndex = getWordIndex(word);
+			wordTagTable[tagIndex][wordIndex]++;
+		}
+	}
+}
+
+int main(int argc, char* argv[]){
+	string trainingFilename = argv[0];
+	string devtFilename = argv[1];
+	string modelFilename = argv[2];
+
+	int totalWordBag = 0;
+	int totalWordType = 0;
+
 	return 0;
 }
