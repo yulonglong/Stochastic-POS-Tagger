45053b3793ebb0083516383469ed0045c12fbe59
Steven Kester <yulonglong2005@yahoo.com>
2014-10-05 22:14:54 +0800

    Added comments and short explanations in Storage.cpp

--- source/Storage.cpp
+++ source/Storage.cpp
@@ -14,19 +14,24 @@ using namespace std;
 
 class Storage{
 public:
+
+	//initialize count table
 	int transitionTagCountTable[TAGSIZE][TAGSIZE];
 	vector< vector<int> > wordTagCountTable;
 	int tagCountTable[TAGSIZE];
 	vector<int> wordCountTable;
 
+	//initialize probability table
 	double tagProbTable[TAGSIZE][TAGSIZE];
 	double tagUnigramProbTable[TAGSIZE];
 	vector< vector<double> > wordTagProbTable;
 	vector<double> wordTagUnigramProbTable;
 
+	//initialize bag of words and word type count
 	int totalWordBag;
 	int totalWordType;
 
+	//initialize maps for tags and words
 	map<string,int> tags;
 	map<int,string> indexTags;
 
@@ -55,6 +60,7 @@ public:
 		totalWordBag+=1;
 	}
 
+	//initialize the tags map with Penn Treebank POS Tags
 	void setTags(){
 		string pennTags[] = { "CC", "CD", "DT", "EX", "FW", "IN",
 				"JJ", "JJR", "JJS", "LS", "MD", "NN", "NNS", "NNP", "NNPS", "PDT",
@@ -69,6 +75,7 @@ public:
 		return;
 	}
 
+	//return tag index given a string of POS tag
 	int getTagIndex(string tag){
 		map<string,int>::iterator it;
 		it = tags.find(tag);
@@ -80,7 +87,7 @@ public:
 		}
 	}
 	
-
+	//insert a word into the word map
 	int insertWord(string word){
 		map<string,int>::iterator it;
 		it = words.find(word);
@@ -98,6 +105,8 @@ public:
 		}
 	}
 
+
+	//convert a string to all lowercase
 	string stringToLower(string word){
 		for(int i=0;i<(int)word.length();i++){
 			word[i] = tolower(word[i]);
@@ -105,11 +114,16 @@ public:
 		return word;
 	}
 
+	//convert the first char of a string to uppercase
 	string stringFirstCharToUpper(string word){
 		word[0] = toupper(word[0]);
 		return word;
 	}
 
+	//try to find a word from the word map, and return its index
+	//if it is not found, try to find the string with all lowercase characters
+	//if it is still not found, try to find the string with first letter uppercase
+	//if it is still not found, return the index of "<UNK>" (unknown word)
 	int getWordIndex(string word){
 		map<string,int>::iterator it;
 		it = words.find(word);
@@ -142,7 +156,7 @@ public:
 		return it->second;
 	}
 
-
+	//get the index of the slash '/' which separates a word and its POS Tags
 	int getDelimiterIndex(string wordAndTag){
 		int index = wordAndTag.length()-1;
 		for(int i=index;i>=0;i--){
@@ -153,6 +167,7 @@ public:
 		return -1;
 	}
 
+	//split a word and a tag from a combined format of "word/tag"
 	void splitWordAndTag(string wordAndTag, string &word, string &tag){
 		int delimIndex = getDelimiterIndex(wordAndTag);
 		word = wordAndTag.substr(0,delimIndex);
@@ -160,6 +175,7 @@ public:
 		return;
 	}
 
+	//export the necessary attributes/tables to file (model_file).
 	bool exportData(string filename){
 		FILE* outfile;
 		outfile = fopen(filename.c_str(),"w+");
@@ -189,6 +205,7 @@ public:
 		return true;
 	}
 
+	//import the necessary attributes/tables from file (model_file).
 	bool importData(string filename){
 		FILE* infile;
 		infile = fopen(filename.c_str(),"r+");
