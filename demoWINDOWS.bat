edb3258e15c3ba4299b709dfb18aa8ebc3376385
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2016-09-29 23:20:20 +0800

    Updated demoWINDOWS script

--- demoWINDOWS.bat
+++ demoWINDOWS.bat
@@ -1,13 +1,14 @@
-echo '==== Compiling Source Code... ===='
+@echo off
+echo ==== Compiling Source Code... ====
 g++ -Wall source/build_tagger.cpp -o build_tagger.exe
 g++ -Wall source/run_tagger.cpp -o run_tagger.exe
-echo '==== Compilation done. ===='
+echo ==== Compilation done. ====
 
-echo '==== Building Model...  ===='
+echo ==== Building Model...  ====
 build_tagger.exe data/sents.train data/sents.devt model_file
-echo '==== Model has been built successfully.  ===='
+echo ==== Model has been built successfully.  ====
 
-echo '==== Running test...  ===='
+echo ==== Running test...  ====
 run_tagger.exe data/sents.test model_file sents.out
-echo '==== Test done.  ===='
-echo '==== The POS-tagged sentences are in sents.out.  ===='
+echo ==== Test done.  ====
+echo ==== The POS-tagged sentences are in sents.out.  ====
